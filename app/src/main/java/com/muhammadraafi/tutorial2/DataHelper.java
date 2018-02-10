0b911fc462d0675724ba7e1bf0afc5772e31c5c8
emrafi <mr.firmanssyah@gmail.com>
2018-02-10 22:26:04 +0700

    First commit

---
+++ app/src/main/java/com/muhammadraafi/tutorial2/DataHelper.java
@@ -0,0 +1,36 @@
+package com.muhammadraafi.tutorial2;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.content.Context;
+import android.util.Log;
+
+/**
+ * Created by uty1 on 25/01/2018.
+ */
+
+public class DataHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "biodatadiri.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public DataHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // TODO Auto-generated method stub
+        String sql = "create table biodata(no integer primary key, nama text null, tgl text null, jk text null, alamat text null);";
+        Log.d("Data", "onCreate: " + sql);
+        db.execSQL(sql);
+        sql = "INSERT INTO biodata (no, nama, tgl, jk, alamat) VALUES ('1001', 'Fathur', '1994-02-03', 'Laki-laki','Jakarta');";
+        db.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase arg0, int arg1, int arg2) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
